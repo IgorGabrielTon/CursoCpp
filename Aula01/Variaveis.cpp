e1c3a96919af4b1d91c39fbf599bb05e52ee1142
Igor Gabriel Tonchichi <99445627+IgorGabrielTon@users.noreply.github.com>
2022-02-26 23:06:55 -0300

    Create Variaveis.cpp

---
+++ Aula01/Variaveis.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+int main()
+{
+    std::cout<<"Hello World";
+
+    //Variaveis
+    int a = 50;
+    double b = 20.2;
+    char c = 'a';
+
+
+    return 0;
+}
