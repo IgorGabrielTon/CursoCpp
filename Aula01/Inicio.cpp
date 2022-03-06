c62a64dd2b8647fefce8525ffa29f4174f7dbb97
Igor Gabriel Tonchichi <99445627+IgorGabrielTon@users.noreply.github.com>
2022-03-06 17:32:12 -0300

    Create Inicio.cpp

---
+++ Aula01/Inicio.cpp
@@ -0,0 +1,17 @@
+//Comentários
+/*Multilinha comentários*/
+
+//Da acesso a namespace std
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "Olá Mundo!\n";
+
+    
+    system("pause");
+    return 0;
+
+}
