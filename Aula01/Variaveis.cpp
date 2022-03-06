a77be1f7b3f575c42b1cc61041e1202b1d1c1636
Igor Gabriel Tonchichi <99445627+IgorGabrielTon@users.noreply.github.com>
2022-03-06 18:29:42 -0300

    Update Variaveis.cpp

--- Aula01/Variaveis.cpp
+++ Aula01/Variaveis.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
-#include <string>
-int main()
-{
-    std::cout<<"Hello World";
-
-    //Variaveis
-    int a = 50;
-    double b = 20.2;
-    char c = 'a';
 
 
-    return 0;
+using namespace std;
+
+int main(){
+
+    cout << "Conhecendo variáveis em C++\n";
+
+    int Idade = 22;
+    float Altura = 1.55;
+    double decimal = 1.45689;
+    char Letra = 'a';
+    string Nome = "Igor";
+
+    cout  << "Sua Idade é: " << Idade << ", e seu nome é " << Nome << endl;
+ return 0;
 }
